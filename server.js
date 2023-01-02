708ba73c814cea9fb72d0610c94d95e3f34a24d2
DaGoblin <s222574652@deakin.edu.au>
2023-01-03 10:49:55 +1100

    Final upload

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 var cors = require("cors");
 // let projectCollection;
-let dbConnect = require("./dbConnect");
+//let dbConnect = require("./dbConnect");
 let projectRoutes = require("./routes/projectRoutes")
 
 
@@ -82,6 +82,20 @@ app.use('/api/projects', projectRoutes)
 //     }
 //   });
 // });
+
+app.get('/addTwoNumbers/:firstNumber/:secondNumber', function(req,res,next){
+  var firstNumber = parseInt(req.params.firstNumber) 
+  var secondNumber = parseInt(req.params.secondNumber)
+  var result = firstNumber + secondNumber || null
+  if(result == null) {
+    res.json({result: result, statusCode: 400}).status(400)
+  }
+  else { res.json({result: result, statusCode: 200}).status(200) } 
+})
+
+
+
+
 
 var port = process.env.port || 3000;
 
